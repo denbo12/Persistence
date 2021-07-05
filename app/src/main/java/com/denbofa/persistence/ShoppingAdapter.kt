737795c0e01dd6b39d4a35cecadd7e0b06ef6a8e
denbo12 <denbiprivate@gmail.com>
2021-07-05 17:50:33 +0100

    Initial commit

---
+++ app/src/main/java/com/denbofa/persistence/ShoppingAdapter.kt
@@ -0,0 +1,28 @@
+package com.denbofa.persistence
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.denbofa.persistence.databinding.ShoppingItemBinding
+
+class ShoppingAdapter(val shoppingItems: List<ShoppingModel>): RecyclerView.Adapter<ShoppingAdapter.ViewHolder>() {
+
+    class ViewHolder(val binding: ShoppingItemBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(shoppingItem: List<ShoppingModel>){
+            binding.category.text = shoppingItem.category
+            binding.description.text = shoppingItem.description
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: ShoppingItemBinding = ShoppingItemBinding.inflate(LayoutInflater.from(parent.context))
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(shoppingItems.get(position))
+    }
+
+    override fun getItemCount() = shoppingItems.size
+}
---
+++ app/src/main/java/com/denbofa/persistence/ShoppingModel.kt
@@ -0,0 +1,6 @@
+package com.denbofa.persistence
+
+data class ShoppingModel(
+    val category: String,
+    val discription: String
+)
