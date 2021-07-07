b128e8e92099709271542cbb61bf0e3a982577e9
denbo12 <denbiprivate@gmail.com>
2021-07-07 15:34:37 +0100

    second commit

---
+++ app/src/main/java/com/denbofa/persistence/ShoppingDatabase.kt
@@ -0,0 +1,10 @@
+package com.denbofa.persistence
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database (entities = arrayOf(ShoppingModel::class), version = 1)
+abstract class ShoppingDatabase: RoomDatabase() {
+
+    abstract fun ShoppingDAO(): ShoppingDAO
+}
